31c2ca70ce53544eecfed112181dd6e94a6fe4b2
Matt <makeshifthidalgo@hotmail.com>
2019-10-09 15:29:32 -0700

    built api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
-// Global app controller
-import num from "./test";
-const x = 23;
-console.log(
-  `I imported ${num} from another module called test! Variable x = ${x}`
-);
+import axios from "axios";
+
+async function getResults(query) {
+  const key = "c2a7cbfe6c5408e8440d4d14477cce0c";
+  try {
+    const res = await axios(
+      `https://www.food2fork.com/api/search?key=${key}&q=${query}`
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (err) {
+    alert(err);
+  }
+}
+getResults("pesto");
