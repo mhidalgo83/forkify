e65845c5fecdb0c680a3eb0d414ad24133b3e166
Matt <makeshifthidalgo@hotmail.com>
2019-10-09 15:45:06 -0700

    constructor built and imported to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,4 @@
-import axios from "axios";
+import Search from "./models/Search";
 
-async function getResults(query) {
-  const key = "c2a7cbfe6c5408e8440d4d14477cce0c";
-  try {
-    const res = await axios(
-      `https://www.food2fork.com/api/search?key=${key}&q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (err) {
-    alert(err);
-  }
-}
-getResults("pesto");
+const search = new Search("pizza");
+search.getResults();
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,20 @@
+import axios from "axios";
 
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const key = "c2a7cbfe6c5408e8440d4d14477cce0c";
+    try {
+      const res = await axios(
+        `https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+      );
+      this.result = res.data.recipes;
+      //console.log(this.result);
+    } catch (err) {
+      alert(err);
+    }
+  }
+}
