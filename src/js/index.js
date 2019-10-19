f3d1d697a7d2c3184a502eeafcbb8739619a99f8
Matt <makeshifthidalgo@hotmail.com>
2019-10-19 14:00:04 -0700

    pagination/button rendering and UI cleanup complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from "./models/Search";
 import * as searchView from "./views/searchView";
-import { elements } from "./views/base";
+import { elements, renderLoader, clearLoader } from "./views/base";
 //Global state of app
 /*
 -Search object
@@ -21,14 +21,26 @@ const controlSearch = async () => {
     // 3) Prepare UI for results
     searchView.clearField();
     searchView.clearInput();
+    renderLoader(elements.searchRes);
+
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) Render results to UI
+    clearLoader();
     searchView.renderResults(state.search.result);
   }
 };
 elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
+});
+elements.searchResPages.addEventListener("click", e => {
+  e.preventDefault();
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    searchView.clearField();
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.renderResults(state.search.result, goToPage);
+  }
 });
